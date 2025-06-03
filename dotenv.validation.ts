a6e97c36af6afeea0d67be3599413a42701ce5bf
Abdelrahman <youssifehab218@gmail.com>
2025-06-03 12:26:20 +0300

    .

--- dotenv.validation.ts
+++ dotenv.validation.ts
@@ -1,4 +1,4 @@
-import { plainToInstance, Transform, Type } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { IsEnum, IsNumber, IsString, validateSync } from 'class-validator';
 
 enum Environment {
@@ -10,51 +10,43 @@ enum Environment {
 
 class EnvironmentVariables {
   @IsEnum(Environment)
-  @Transform(({ value }) => value?.trim())
   NODE_ENV: Environment;
 
-  @Type(() => Number)
-  @IsNumber({}, { message: 'PORT must be a number' })
+  @IsNumber()
   PORT: number;
 
-  @Transform(({ value }) => value?.trim())
-  @IsString({ message: 'SECRET must be a string' })
-  SECRET: string;
-
-  @Transform(({ value }) => value?.trim())
-  @IsString({ message: 'DB_HOST must be a string' })
-  DB_HOST: string;
-
-  @Type(() => Number)
-  @IsNumber({}, { message: 'DB_PORT must be a number' })
+  @IsNumber()
   DB_PORT: number;
 
-  @Transform(({ value }) => value?.trim())
-  @IsString({ message: 'DB_USERNAME must be a string' })
-  DB_USERNAME: string;
+  @IsString()
+  DB_HOST: string;
 
-  @Transform(({ value }) => value?.trim())
-  @IsString({ message: 'DB_PASSWORD must be a string' })
-  DB_PASSWORD: string;
+  @IsString()
+  USERNAME: string;
 
-  @Transform(({ value }) => value?.trim())
-  @IsString({ message: 'DB_NAME must be a string' })
+  @IsString()
+  PASSWORD: string;
+
+  @IsString()
   DB_NAME: string;
+
+  @IsString()
+  SECRET: string;
 }
 
 export function validate(config: Record<string, unknown>) {
+  // console.log('config ', config);
   const validatedConfig = plainToInstance(EnvironmentVariables, config, {
     enableImplicitConversion: true,
   });
+  // console.log(validatedConfig);
 
   const errors = validateSync(validatedConfig, {
     skipMissingProperties: false,
   });
 
   if (errors.length > 0) {
-    console.error(JSON.stringify(errors, null, 2));
-    throw new Error('Config validation failed. See error output above.');
+    throw new Error(errors.toString());
   }
-
   return validatedConfig;
 }
