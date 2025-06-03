0f0f8b8cc246ccc59092b16ab4f6d5d668165673
Abdelrahman <youssifehab218@gmail.com>
2025-06-03 11:39:18 +0300

    remove console.log

--- src/main.ts
+++ src/main.ts
@@ -43,11 +43,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('port'));
 
-  console.log(
-    configService.get<string>('NODE_ENV'),
-    configService.get<number>('port'),
-  );
-
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
